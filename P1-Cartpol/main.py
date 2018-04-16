6226d770bfa36a6918069fbab4c2b2dc3e255f6d
xHe <xuan.he@columbia.edu>
2018-04-16 12:30:54 -0700

    setting up environment

---
+++ P1-Cartpol/main.py
@@ -0,0 +1,50 @@
+import gym
+import numpy as np
+env = gym.make('CartPole-v0')
+env.reset()
+
+class Solution():
+    def __init__(self, env):
+        self.env = env
+    
+    def run_episode(self):
+        observation = env.reset()
+        totalreward = 0
+        for _ in range(200):
+            action = self.obs_to_action(observation)
+            observation, reward, done, info = env.step(action)
+            totalreward += reward
+            if done:
+                break
+        return totalreward   
+
+    def obs_to_action(self, observation):
+        return env.action_space.sample()
+
+class SolRandom(Solution):
+    def __init__(self, env):
+        super().__init__(env)
+        self.parameters = np.random.rand(4) * 2 -1
+
+    def obs_to_action(self, observation):
+        action = 0 if np.matmul(self.parameters, observation) < 0 else 1
+        return action
+
+    def run(self):
+        bestparams = None
+        bestreward = 0
+        for laps in range(10000):
+            self.parameters = np.random.rand(4) *2 -1
+            reward = self.run_episode()
+            if reward > bestreward:
+                bestparams = self.parameters
+                if reward == 200:
+                    print("solved")
+                    return laps
+
+
+sol = SolRandom(env)
+sol.run()
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
