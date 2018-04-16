d0c753c0c751da2b21e456c3751df0dc02a32f52
xHe <xuan.he@columbia.edu>
2018-04-16 13:22:17 -0700

    forked from project 2
    https://github.com/taivop/joke-dataset

--- P1-Cartpol/main.py
+++ P1-Cartpol/main.py
@@ -40,11 +40,44 @@ class SolRandom(Solution):
                 bestparams = self.parameters
                 if reward == 200:
                     print("solved")
-                    return laps
+                    return laps, self.parameters
+
+# TODO : improve hill climbing
+class SolHillClimb(Solution):
+    def __init__(self, env):
+        super().__init__(env)
+        self.parameters = np.random.rand(4) * 2 -1
+        self.bestparams = self.parameters
+        self.alpha = 0.1
+
+    def obs_to_action(self, observation):
+        action = 0 if np.matmul(self.parameters, observation) < 0 else 1
+        return action
+
+    def run(self):
+        bestreward = 0
+        for laps in range(10000):
+            self.parameters = self.bestparams + (np.random.rand(4) *2 -1) * self.alpha
+            reward = self.run_episode()
+            if reward > bestreward:
+                self.bestparams = self.parameters
+                bestreward = reward
+                if reward == 200:
+                    print("solved")
+                    return laps, self.parameters
+
+# Finish Policy Gradient
+class SolPolicyGrad(Solution):
+    def __init__(self, env):
+        super.__init__(env)
 
 
 sol = SolRandom(env)
-sol.run()
+print(sol.run())
+
+
+sol = SolHillClimb(env)
+print(sol.run())
 # for _ in range(1000):
 #     env.render()
 #     env.step(env.action_space.sample()) # take a random action
