5aba793dcd3ba5aa6d413d7214aaa8b5689e23a7
xHe <xuan.he@columbia.edu>
2018-04-17 16:48:22 -0700

    move old lunar lander repo

---
+++ Box2D/LunarLander/lunarLanderV2.py
@@ -0,0 +1,207 @@
+import gym
+import numpy as np
+from keras.optimizers import Adam
+from keras import backend as K
+
+import random
+import pandas as pd
+
+import os
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import *
+
+seed = 16
+env = gym.make('LunarLander-v2')
+np.random.seed(seed)
+random.seed(seed)
+env.seed(seed)
+env = gym.wrappers.Monitor(env, './monitor/', force=True)
+
+def huber_loss(y_true, y_pred):
+    err = y_pred - y_true
+    return K.mean(K.sqrt(1+K.square(err))-1, axis=-1 )
+
+class Experience(object):
+    def __init__(self, maxsize=500000):
+        self.maxsize = maxsize
+        self.state  = []
+        self.action  = []
+        self.reward  = []
+        self.state1 = []
+        self.done = []
+        self.curr_size = 0
+
+    def save(self, s, a, r, s1, _done):
+        self.state.append(s)
+        self.action.append(a)
+        self.reward.append(r)
+        self.state1.append(s1)
+        self.done.append(_done)
+        self.curr_size += 1
+
+        if self.curr_size > self.maxsize:
+            self.state.pop(0)
+            self.action.pop(0)
+            self.reward.pop(0)
+            self.state1.pop(0)
+            self.done.pop(0)
+            self.curr_size -= 1
+
+    def sample(self, n):
+        N = self.curr_size
+        n = min(n, N)
+        idx = np.random.choice(N, n, replace=False)
+        s    = [self.state[i]  for i in idx]
+        a    = [self.action[i] for i in idx]
+        r    = [self.reward[i] for i in idx]
+        s1   = [self.state1[i] for i in idx]
+        done = [self.done[i]   for i in idx]
+        return (s, a, r, s1, done)
+
+
+class SolutionAgent(object):
+
+    def __init__(self, state_dim, action_dim, decay=0.985,
+                buffersize=500000, samplesize=64, minsamples=10000,
+                gamma=0.99,updateTargetFreq=600):
+
+        self.state_dim  = state_dim
+        self.action_dim = action_dim
+        self.exp        = Experience(maxsize=buffersize)
+        self.epsilon    = 1.0
+        self.minsamples = minsamples
+        self.samplesize = samplesize
+        self.decay      = decay
+        self.gamma      = gamma
+        self.updateTargetFreq = updateTargetFreq
+        self.steps = 0
+        self.model        = self.createModel()
+        self.target_model = self.createModel()
+
+    def createModel(self):
+        model = Sequential()
+        model.add(Dense(50, input_dim=self.state_dim))
+        model.add(Activation("relu"))
+        model.add(Dense(40))
+        model.add(Activation("relu"))
+        model.add(Dense(self.action_dim))
+        model.add(Activation('linear'))
+        model.compile(loss=huber_loss, optimizer=Adam(lr=0.0005))
+        return model
+
+    def actionSelection(self, curState, testmode=False):
+        if not testmode:
+            if (np.random.random() <= self.epsilon):
+                return random.randint(0, self.action_dim-1)
+        s = np.array([curState])
+        return np.argmax(self.model.predict(s)[0])
+
+    def evaluate(self, prevState, action, reward, curState, done):
+        self.exp.save(prevState, action, reward, curState, done)
+        if done:
+            self.epsilon *= self.decay
+        if self.steps % self.updateTargetFreq == 0:
+            self.target_model.set_weights(self.model.get_weights())
+        self.steps += 1
+
+    def learn(self):
+        if self.exp.curr_size <= self.minsamples:
+            return 0.0
+        X, y = self.get_training_data()
+        loss = self.model.train_on_batch(X,y)
+        return loss
+
+    def get_training_data(self):
+        s, a, r, s1, done = self.exp.sample(self.samplesize)
+        s  = np.array(s)
+        s1 = np.array(s1)
+        q  = self.model.predict(s)
+        q1 = self.target_model.predict(s1)
+        X = s
+        y = np.zeros((self.samplesize, self.action_dim))
+        for idx in xrange(self.samplesize):
+            reward = r[idx]
+            action = a[idx]
+            target = q[idx]
+            target_for_action = reward
+            if not done[idx]:
+                target_for_action += ( self.gamma*max(q1[idx]) )
+            target[action] = target_for_action
+            y[idx, :] = target
+        return X, y
+
+
+
+agent = SolutionAgent(state_dim=env.observation_space.shape[0], action_dim=env.action_space.n,
+                    decay=0.985, buffersize=500000, samplesize=32, minsamples=1000,
+                    gamma=0.99, updateTargetFreq=600)
+
+
+print "-------------------------Training Mode-----------------------"
+numOfEpisodes = 1000
+testmode = False
+window_avg = [0] * 100
+max_mean = 0.0
+train_file = open("train.csv", "wb")
+for idx in xrange(numOfEpisodes):
+    currSatet = env.reset()
+    done = False
+    loss = 0.0
+    numSteps = 0
+    total_reward = 0.0
+
+    while True:
+        numSteps += 1
+        action = agent.actionSelection(currSatet, testmode=False)
+        prevState = currSatet
+        currSatet, reward, done, info = env.step(action)
+
+        total_reward += reward
+
+        if not testmode:
+            agent.evaluate(prevState, action, reward, currSatet, done)
+            loss += agent.learn()
+        if done: break
+
+    window_avg.pop(0)
+    window_avg.append(total_reward)
+    curmean = np.mean(window_avg)
+    if curmean > max_mean:
+        max_mean = curmean
+    # if max_mean > 200: break
+    print 'Episode NO.%d:  Reward = %.2f, Mean: %.2f Loss = %f, Steps = %d' % (idx+1,total_reward,curmean,loss,numSteps)
+    train_file.write(total_reward)
+    train_file.write('\n')
+
+train_file.close()
+print "------------End of Training:   max mean %.2f-----------------" % (max_mean)
+print "-------------------------Test Mode-----------------------"
+
+numOfEpisodes = 1000
+testmode = True
+test_file = open("test.csv", "wb")
+
+for idx in xrange(numOfEpisodes):
+    currSatet = env.reset()
+    done = False
+    loss = 0.0
+    numSteps = 0
+    total_reward = 0.0
+
+    while True:
+        numSteps += 1
+        action = agent.actionSelection(currSatet, testmode)
+        prevState = currSatet
+        currSatet, reward, done, info = env.step(action)
+        total_reward += reward
+        if done: break
+    test_file.write(str(total_reward))
+    test_file.write('\n')
+    print "Reward: %.2f" % (total_reward)
+
+env.close()
+
+gym.upload('./monitor', api_key="")
+
+
--- P1-Cartpol/cartpole.py
+++ P1-Cartpol/cartpole.py
@@ -1,140 +1,203 @@
-"""
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
-"""
-
-import math
 import gym
-from gym import spaces, logger
-from gym.utils import seeding
 import numpy as np
+import tensorflow as tf
+import random
 
-class CartPoleEnv(gym.Env):
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 50
-    }
+env = gym.make('CartPole-v0')
+env.monitor.start('cartpole/', force=True)
+env.reset()
 
-    def __init__(self):
-        self.gravity = 9.8
-        self.masscart = 1.0
-        self.masspole = 0.1
-        self.total_mass = (self.masspole + self.masscart)
-        self.length = 0.5 # actually half the pole's length
-        self.polemass_length = (self.masspole * self.length)
-        self.force_mag = 10.0
-        self.tau = 0.02  # seconds between state updates
+class Solution():
+    def __init__(self, env):
+        self.env = env
+    
+    def run_episode(self):
+        observation = self.env.reset()
+        totalreward = 0
+        for _ in range(200):
+            action = self.obs_to_action(observation)
+            observation, reward, done, info = env.step(action)
+            totalreward += reward
+            if done:
+                break
+        return totalreward   
 
-        # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 2.4
+    def obs_to_action(self, observation):
+        return env.action_space.sample()
 
-        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
-        high = np.array([
-            self.x_threshold * 2,
-            np.finfo(np.float32).max,
-            self.theta_threshold_radians * 2,
-            np.finfo(np.float32).max])
+class SolRandom(Solution):
+    def __init__(self, env):
+        super().__init__(env)
+        self.parameters = np.random.rand(4) * 2 -1
 
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-high, high)
+    def obs_to_action(self, observation):
+        action = 0 if np.matmul(self.parameters, observation) < 0 else 1
+        return action
 
-        self.seed()
-        self.viewer = None
-        self.state = None
+    def run(self):
+        bestparams = None
+        bestreward = 0
+        for laps in range(10000):
+            self.parameters = np.random.rand(4) *2 -1
+            reward = self.run_episode()
+            if reward > bestreward:
+                bestparams = self.parameters
+                if reward == 200:
+                    print("solved")
+                    return laps, self.parameters
 
-        self.steps_beyond_done = None
+# TODO : improve hill climbing
+class SolHillClimb(Solution):
+    def __init__(self, env):
+        super().__init__(env)
+        self.parameters = np.random.rand(4) * 2 -1
+        self.bestparams = self.parameters
+        self.alpha = 0.1
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+    def obs_to_action(self, observation):
+        action = 0 if np.matmul(self.parameters, observation) < 0 else 1
+        return action
 
-    def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        state = self.state
-        x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action==1 else -self.force_mag
-        costheta = math.cos(theta)
-        sintheta = math.sin(theta)
-        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        x  = x + self.tau * x_dot
-        x_dot = x_dot + self.tau * xacc
-        theta = theta + self.tau * theta_dot
-        theta_dot = theta_dot + self.tau * thetaacc
-        self.state = (x,x_dot,theta,theta_dot)
-        done =  x < -self.x_threshold \
-                or x > self.x_threshold \
-                or theta < -self.theta_threshold_radians \
-                or theta > self.theta_threshold_radians
-        done = bool(done)
+    def run(self):
+        bestreward = 0
+        for laps in range(10000):
+            self.parameters = self.bestparams + (np.random.rand(4) *2 -1) * self.alpha
+            reward = self.run_episode()
+            if reward > bestreward:
+                self.bestparams = self.parameters
+                bestreward = reward
+                if reward == 200:
+                    print("solved")
+                    return laps, self.parameters
 
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-            self.steps_beyond_done += 1
-            reward = 0.0
+# Finish Policy Gradient
+class SolPolicyGrad(Solution):
+    def __init__(self, env):
+        super.__init__(env)
 
-        return np.array(self.state), reward, done, {}
+    def run_episode(self):
+        pl_calculated, pl_state, pl_action,  pl_adv, pl_optimizer = policy_grad
+        vl_calculated, vl_state, vl_newvals, vl_optimizer, vl_loss = value_grad
+        obs = self.env.reset()
+        totalreward = 0
+        states = []
+        actions = []
+        advs = []
+        transitions = []
+        update_vals = []
 
-    def reset(self):
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.steps_beyond_done = None
-        return np.array(self.state)
+        for _ in range(200):
+            # calculate policy
+            obs_vector = np.expand_dims(obs, axis=0)
+            probs = sess.run(pl_calculated, feed_dict={pl_state: obs_vector})
+            action = 0 if random.uniform(0, 1) < probs[0][0] else 1
+            # record the transition
+            states.append(obs)
+            actionblank = np.zeros(2)
+            actionblank = 1
+            actions.append(actionblank)
 
-    def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
+            # take the action in env
+            old_obs = obs
+            obs, reward, done, info = env.step(action)
+            totalreward += reward
 
-        world_width = self.x_threshold*2
-        scale = screen_width/world_width
-        carty = 100 # TOP OF CART
-        polewidth = 10.0
-        polelen = scale * 1.0
-        cartwidth = 50.0
-        cartheight = 30.0
+            if done:
+                break
 
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
-            axleoffset =cartheight/4.0
-            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            self.carttrans = rendering.Transform()
-            cart.add_attr(self.carttrans)
-            self.viewer.add_geom(cart)
-            l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-            pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            pole.set_color(.8,.6,.4)
-            self.poletrans = rendering.Transform(translation=(0, axleoffset))
-            pole.add_attr(self.poletrans)
-            pole.add_attr(self.carttrans)
-            self.viewer.add_geom(pole)
-            self.axle = rendering.make_circle(polewidth/2)
-            self.axle.add_attr(self.poletrans)
-            self.axle.add_attr(self.carttrans)
-            self.axle.set_color(.5,.5,.8)
-            self.viewer.add_geom(self.axle)
-            self.track = rendering.Line((0,carty), (screen_width,carty))
-            self.track.set_color(0,0,0)
-            self.viewer.add_geom(self.track)
+        for index, trans in enumerate(transitions):
+            obs, action, reward = trans
 
-        if self.state is None: return None
+            future_reward = 0
+            future_transitions = len(transitions) - index
+            decrease = 1
 
-        x = self.state
-        cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
-        self.carttrans.set_translation(cartx, carty)
-        self.poletrans.set_rotation(-x[2])
+            for index2 in range(future_transitions):
+                future_reward += transitions[(index2) + index][2] * decrease
+                decrease = decrease * 0.97
 
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+            obs_vector = np.expand_dims(obs, axis=0)
+            currentval = sess.run(vl_calculated, feed_dict={vl_state: obs_vector})[0][0]
 
-    def close(self):
-        if self.viewer: self.viewer.close()
+            advs.append(future_reward - currentval)
+            update_vals.append(future_reward)
+
+        update_vals_vector = np.expand_dims(update_vals, axis=1)
+        sess.run(vl_optimizerm, feed_dict={vl_state: states, vl_newvals: update_vals_vector})
+
+        advs_vector = np.expand_dims(advs, axis=1)
+        sess.run(pl_optimizerm, feed_dict={pl_state: states, pl_adv: advs_vector, pl_action: actions})
+
+        return totalreward
+
+
+    def run(self):
+        policy_grad = policy_gradient()
+        value_grad = value_gradient()
+        sess = tf.InteractiveSession()
+        sess.run(tf.initialize_all_variables())
+
+        for i in range(2000):
+            reward = run_episode(env, policy_grad, value_grad, sess)
+            if reward > 200:
+                print( "reward 200")
+                print i 
+                return i
+        
+        t = 0
+        for _ in range(1000):
+            reward = run_episode(env, policy_grad, value_grad, sess)
+            t += reward
+        
+        return float(t)/1000
+
+def policy_gradient():
+    with tf.variable_scope("policy"):
+        params = tf.get_variable("policy_parameters", [4,2])
+        state = tf.placeholder("float", [None, 4])
+        actions = tf.placeholder("float", [None, 2])
+        
+        adv = tf.placeholder("float", [None, 1])
+
+        linear = tf.matmul(state, params)
+        probs = tf.nn.softmax(linear)
+        good_probs = tf.reduce_sum(tf.mul(probs, actions), reduction_indices=[1])
+        
+        log_probs = tf.log(good_probs)
+        
+        eligibility =  log_probs * adv
+        loss = -tf.reduce_sum(eligibility)
+        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
+
+        return probs, state, action, adv, optimize
+
+def value_gradient():
+    with tf.variable_scope("value"):
+        state = tf.placeholder("float", [None, 4])
+        w1 = tf.get_variable("w1", [4, 10])
+        b1 = tf.get_variable("b1", [10])
+        h1 = tf.nn.relu(tf.matmul(state, w1) + b1)
+
+        w2 = tf.get_variable("w2", [10, 1])
+        b2 = tf.get_variable("b2", [1])
+        calculated = tf.matmul(h1, w2) + b2
+        newvals = tf.placeholder("float", [None, 1])
+        diffs = calculated - newvals
+        loss = tf.nn.l2_loss (diffs)
+
+        optimizer = tf.train.AdamOptimizer(0.1).minimize(loss)
+
+        return calculated, state, newvals, optimizer, loss
+
+
+sol = SolRandom(env)
+print(sol.run())
+
+
+sol = SolHillClimb(env)
+print(sol.run())
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+
+env.monitor.close()
